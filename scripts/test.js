5f00cea5b05aa60376f9751fcd04673171891ddf
noieeen <tharnnn1401@gmail.com>
2023-09-06 03:58:43 +0700

    update
    https://youtu.be/4EBhkFWN16w?list=PLB9ERRiXvIRV-ctGm5UEG4I6AKnyoTTWH&t=4466

--- scripts/test.js
+++ scripts/test.js
@@ -3,13 +3,6 @@ import http from "k6/http";
 export const options = {
   vus: 10,
   duration: "60s",
-//   iterations: 1,
-
-  //   stages: [
-  // { duration: "10s", target: 10 },
-  //   { duration: '1m30s', target: 10 },
-  //   { duration: '20s', target: 0 },
-  //   ],
 };
 
 export default function () {
