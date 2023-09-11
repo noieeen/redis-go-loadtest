867e10f889bd1db65f735b09dbaca996b57ce3ac
noieeen <tharnnn1401@gmail.com>
2023-09-11 23:25:45 +0700

    redis integrade

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"fmt"
+	"goredis/repositories"
+
+	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	app := fiber.New()
+	// app := fiber.New()
 
-	app.Get("/hello", func(c *fiber.Ctx) error {
-		// time.Sleep(time.Millisecond * 10)
-		return c.SendString("Hello, world")
+	// app.Get("/hello", func(c *fiber.Ctx) error {
+	// 	// time.Sleep(time.Millisecond * 10)
+	// 	return c.SendString("Hello, world")
+	// })
+
+	// app.Listen(":8000")
+	db := initDatabase()
+	redisClient := initRedis()
+
+	// productRepo := repositories.NewProductRepositoryDB(db)
+	productRepo := repositories.NewProductRepositoryRedis(db, redisClient)
+
+	products, err := productRepo.GetProducts()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(products)
+}
+
+func initDatabase() *gorm.DB {
+	dial := mysql.Open("root:password@tcp(localhost:3306)/infinitas")
+	db, err := gorm.Open(dial, &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func initRedis() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
 	})
-
-	app.Listen(":8000")
 }
---
+++ repositories/product_redis.go
@@ -0,0 +1,57 @@
+package repositories
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"gorm.io/gorm"
+)
+
+type ProductRepositoryRedis struct {
+	db          *gorm.DB
+	redisClient *redis.Client
+}
+
+func NewProductRepositoryRedis(db *gorm.DB, redisClient *redis.Client) ProductRepositoryRedis {
+	db.AutoMigrate()
+	mockData(db)
+	return ProductRepositoryRedis{db, redisClient}
+}
+
+func (r ProductRepositoryRedis) GetProducts() (products []product, err error) {
+	// Redis Key
+	key := "repository::GetProducts"
+
+	// Redis GetProducts
+	productJson, err := r.redisClient.Get(context.Background(), key).Result()
+	if err == nil {
+		err = json.Unmarshal([]byte(productJson), &products)
+		if err == nil {
+			fmt.Println("Redis")
+			return products, nil
+		}
+	}
+
+	// Database
+	err = r.db.Order("quantity desc").Limit(30).Find(&products).Error
+	if err != nil {
+		return nil, err
+	}
+
+	// Redis Set
+	data, err := json.Marshal(products)
+	if err != nil {
+		return nil, err
+	}
+	err = r.redisClient.Set(context.Background(), key, string(data), time.Second*10).Err()
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Database")
+
+	return products, nil
+}
